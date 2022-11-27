000b8c755f32c1b6a7bfbae5d7afe0ca66f75080
Sampath <sampathvatturi@gmail.com>
2022-11-27 16:34:51 +0530

    changes in db

--- config/config.js
+++ config/config.js
@@ -1,10 +1,19 @@
 const mysql = require('mysql');
+// const con = mysql.createConnection({
+//     database:'department_appr',
+//     host: '127.0.0.1',
+//     user: 'root',
+//     password: '',
+//     port:'3310'
+// });
+
+
 const con = mysql.createConnection({
-    database:'department_appr',
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    port:'3310'
+    database:'test',
+    host: 'database-2.cbc4xnqy8kql.us-west-2.rds.amazonaws.com',
+    user: 'admin',
+    password: 'password',
+    port:'3306'
 });
 
 con.connect(function(err) {
